0ebf0c4a665967169a1595fd90e38feba3932e97
RazielRiMo <racielmiguel111@gmail.com>
2026-06-03 20:14:05 -0500

    se agrega sensor de nivel optico en 3 alturas distintas para diferenciar entre lleno , medio y critico

--- STM32_ENVASADORA/src/main.cpp
+++ STM32_ENVASADORA/src/main.cpp
@@ -1,46 +1,54 @@
 //LIBRERIAS
+
 #include <Arduino.h>
-#include <ArduinoJson.h>
+#include <ArduinoJson.h>//para los paquetes
 #include <STM32FreeRTOS.h>
 #include <task.h>
 #include <semphr.h>
 #include <HardwareSerial.h>
 #include <timeLib.h>
 
-//definiciones
+//definiciones de pines
 
-#define TRIGGER_PIN 6
-#define ECHO_PIN 7
-#define muestrasprox 10
+#define TRIGGER_PIN 6 //trigger del ultrasonido
+#define ECHO_PIN 7 //echo del ultrasonido
+
+#define NIV1 8 //nivel alto o lleno el tanque
+#define NIV2 9 //nivel medio o seguro para sequir
+#define NIV3 10 //nivel bajo o critico
+
+
+//definicion de variables
+
+#define muestrasprox 10 //tamaño de muestras de distancia
 
 //declaracion de Comunicacion UART
 
-HardwareSerial hserial(PA3, PA2);//PA3 RX, PA2 TX
+HardwareSerial hserial(PA3, PA2);//PA3 RX, PA2 TX -----> para comunicacion con la esp
 
 //tareas
 
-void sensor_nivel(void *pvParameters);
-void comunicacion(void *pvParameters);
-void motor_cinta(void *pvParameters);
+void comunicacion(void *pvParameters); //comunica a la esp los datos actuales
+void motor_cinta(void *pvParameters); //controla la velocidad del motor en la cinta
 
 //tareas sensor de proximidad
 
-void run_trigger(void *pvParameters);
-void readUltrasonic(void *pvParameters);
-void run_timer(void *pvParameters);
-void procesarprox(void *pvParameters);
+void run_trigger(void *pvParameters); //dispara el trigger
+void readUltrasonic(void *pvParameters); //calcula distancia y la envia a la cola
+void run_timer(void *pvParameters); //empieza a contar para calcular distancia
+void procesarprox(void *pvParameters); //recoge el dato de la cola y lo promedia
 
-//interrupciones
+//interrupciones sensor de proximidad
 
 void isr_echo(void);
+
+//interrupciones sensor de nivel
+
+void isr_sen(void);
 
 //objetos
 
 int crcal(String str);
-
-//objetos auxiliares que se retiraran cuando se decida que sensor usar
-
-float niv ();
 
 //declaracion de estructura
 
@@ -62,13 +70,12 @@ String Jsonstr;
 //declaracion de listas
 
 QueueHandle_t senprox; //cola para promedio proximidad
-QueueHandle_t senniv;  //cola para promedio nivel
 QueueHandle_t proxref; //cola para proximidad refinada
 QueueHandle_t nivref;  //cola para nivel refinado
 
 //semaforos
 
-SemaphoreHandle_t sem1, sem2, mutex, res, inicio;
+SemaphoreHandle_t sem1, sem2, niv;
 
 //variables proximidad
 
@@ -86,16 +93,22 @@ void setup() {
 
   Serial.begin(115200);
 
-  //definicion de pines
+  //definicion de pines ultrasonido
 
   pinMode(TRIGGER_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
+
+  //definicion de pines nivel
+
+  pinMode(NIV1, INPUT);
+  pinMode(NIV2, INPUT);
+  pinMode(NIV3, INPUT);
 
   //Wire.begin(); //descomentar si hay sensor i2c
 
   //inicializo el json
 
-  datos["niv"] = 0.0f;
+  datos["niv"] = 3;
   datos["prox"] = 0.0f;
   datos["vel"] = 0.0f;
   datos["pos"] = 0;
@@ -104,25 +117,33 @@ void setup() {
   //configurar listas
 
   senprox = xQueueCreate(10, sizeof(long));
-  senniv = xQueueCreate(10, sizeof(float));
   proxref = xQueueCreate(1, sizeof(float));
   nivref = xQueueCreate(1, sizeof(float));
 
   //verificar la creacion de las listas
 
   configASSERT(senprox !=NULL);
-  configASSERT(senniv !=NULL);
   configASSERT(proxref !=NULL);
   configASSERT(nivref !=NULL);
 
-  //definicion de semaforos
+  //definicion de semaforos ultrasonido
 
   sem1 = xSemaphoreCreateBinary();
   sem2 = xSemaphoreCreateBinary();
 
-  //se definen interrupciones
+  //definicion de semaforos nivel
+
+  niv = xSemaphoreCreateBinary();
+
+  //se definen interrupciones ultrasonido
 
   attachInterrupt(digitalPinToInterrupt(ECHO_PIN), isr_echo, CHANGE);
+
+  //definicion de interrupciones nivel cualquiera que cambie activa la interrupcion
+
+  attachInterrupt(digitalPinToInterrupt(NIV1), isr_sen, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(NIV2), isr_sen, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(NIV3), isr_sen, CHANGE);
 
   //se definen las tareas
 
@@ -133,7 +154,6 @@ void setup() {
   xTaskCreate(run_timer, "Run Timer", 128, NULL, 3, NULL);
   xTaskCreate(procesarprox, "proceso de proximidad", 128, NULL, 3, NULL);
 
-  xTaskCreate(sensor_nivel, "comprobacion-nivel", 256, NULL, 2,NULL);
   xTaskCreate(comunicacion, "comunicacion_UART", 256, NULL, 1, NULL);
   xTaskCreate(motor_cinta, "velocidad_cinta", 256, NULL, 4, NULL);
   
@@ -145,10 +165,6 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 }
-
-//funciones que se eliminaran al colocar sensores
-
-float niv (){return random(0.0, 10.0);}
 
 //objetos realmente nesesarios
 
@@ -239,11 +255,13 @@ void readUltrasonic(void *pvParameters)
   }
 }
 
+//proceso de procesamiento del sensor de proximidad
+
 void procesarprox(void *pvParameters){
-  long buf[muestrasprox] = {0.0f};
-  int index = 0;
-  long sum = 0.0f;
-  uint8_t cuenta = 0;
+  long buf[muestrasprox] = {0.0f};//buffer para poner los datos tomados
+  int index = 0;//index que indica que dato borrar y cual reemplazar
+  long sum = 0.0f;//variable para guardar la suma de todos los valores
+  uint8_t cuenta = 0;//cuenta que sirve para saber 
   long distance;
   while(1){
     if(xQueueReceive(senprox, &distance, portMAX_DELAY) == pdTRUE){
@@ -256,6 +274,37 @@ void procesarprox(void *pvParameters){
       float basura;
       xQueueReceive(proxref, &basura, 0);
       xQueueSend(proxref, &promedio, 0);
+    }
+  }
+}
+
+//interrupcion actualizacion de nivel de agua
+
+void isr_sen(void){
+  //liberar el semaforo para que la tarea de lectura pueda calcular el nivel del agua
+  BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+  ena = true;
+  xSemaphoreGiveFromISR(niv, &xHigherPriorityTaskWoken);
+  portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+}
+
+//tarea para calcular el nivel del agua
+void run_timer(void *pvParameters){
+  int pos = 3;
+  while(1)
+  {
+    if((xSemaphoreTake(niv, portMAX_DELAY) == pdTRUE) && !ena){
+      if (digitalRead(NIV3) == LOW) {
+        pos = 3;
+      } else if (digitalRead(NIV2) == LOW) {
+        pos = 2;
+      } else if (digitalRead(NIV1) == LOW) {
+        pos = 1;
+      }
+      
+      int basura;
+      xQueueReceive(nivref, &basura, 0);
+      xQueueSend(nivref, &pos, 0);
     }
   }
 }
